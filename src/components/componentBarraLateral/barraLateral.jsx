8fbeab6885ae4d00f515ba8f48a5cdb5bc25be1e
rodrikraus <rodrikraus10@hotmail.com>
2023-05-20 19:54:07 -0300

    Carrito ahora funciona y usa el api correctamente.

--- src/components/componentBarraLateral/barraLateral.jsx
+++ src/components/componentBarraLateral/barraLateral.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Tab, Nav, Card } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Popover from 'react-bootstrap/Popover';
 
 const BarraLateral = () => {
   const [activeTab, setActiveTab] = useState(''); //active tab, se inicializa en '', y la funcion setActiveTab la updatea
   const [categories, setCategories] = useState([]); //categories, se inicializa como un arreglo vacio, y la funcion setCategories lo updatea
   const [products, setProducts] = useState([]); //products, se inicializa como un arreglo vacio, y la funcion setProducts lo updatea
   const [cartItems, setCartItems] = useState([]); //cartItems, se inicializa como un arreglo vacio, y la funcion setCartItems lo updatea
+  const [customerEmail, setCustomerEmail] = useState(''); //el mail se inicializa en vacio, se guarda en customerEmail y se modifica con setCustomerEmail
 
+  const handleEmailChange = (event) => {
+    setCustomerEmail(event.target.value);
+  };
 
   useEffect(() => {
     fetch('https://iot-impact-laravel-dl17wkn66-iot-impact.vercel.app/rest/categories')
@@ -35,7 +42,83 @@ const BarraLateral = () => {
     }
   };
 
-  //Mostrar las cards
+  //Funcion que se ejecuta luego de clickear aceptar compra en el popover del carrito
+  const aceptarCompra = () => {
+    const orderData = {
+      customer_email: customerEmail,
+      total_amount: cartItems.reduce(
+        (total, product) => total + parseFloat(product.price),
+        0
+      ),
+      items: cartItems.map((product) => ({
+        product_id: product.id,
+        quantity: 1, // Adjust quantity as needed
+        individual_price: parseFloat(product.price),
+      })),
+    };
+  
+    fetch('https://iot-impact-laravel-dl17wkn66-iot-impact.vercel.app/rest/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(orderData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Muestro mensaje de exito
+        window.confirm('Compra exitosa.');
+        console.log('Order placed successfully:', data);
+        // Resetear el carrito y el mail
+        setCartItems([]);
+        setCustomerEmail(['']);
+      })
+      .catch((error) => {
+        // Muestro mensaje de error
+        console.error('Error placing order:', error);
+      });
+  };
+
+  const popoverCarrito = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Productos agregados:</Popover.Header>
+      <Popover.Body>
+        {cartItems.map((productoAgregado) => (
+          <div key={productoAgregado.id}>
+            <span>{productoAgregado.name} ${productoAgregado.price}</span>
+            <Button
+              variant="danger"
+              size="sm"
+              onClick={() => removeProductFromCart(productoAgregado.id)}
+            >
+              -
+            </Button>
+          </div>
+        ))}
+        <div>
+          Precio total: ${cartItems.reduce((total, product) => total + parseFloat(product.price), 0)}
+        </div>
+        <Button variant="primary" onClick={aceptarCompra}>
+          Comprar
+        </Button>
+        <input
+          type="text"
+          value={customerEmail}
+          onChange={handleEmailChange}
+          placeholder="Ingrese su email"
+        />
+      </Popover.Body>
+    </Popover>
+  );
+  
+  //Funcion que se ejecuta al clickear el "-" en el popover del carrito
+  const removeProductFromCart = (productId) => {
+    setCartItems((prevCartItems) =>
+      prevCartItems.filter((product) => product.id !== productId)
+    );
+  };
+
+  //Funcion que renderiza las cards de los productos
   const renderCards = () => {
     const category = categories.find((cat) => cat.name === activeTab);
 
@@ -59,10 +142,12 @@ const BarraLateral = () => {
   };
 
   return (
-    //Muestro el carrito
     <div>
+      
 
-      <p>Cart Items: {cartItems.length}</p>
+      <OverlayTrigger trigger="click" placement="right" overlay={popoverCarrito}>
+        <Button variant="success">Carrito: {cartItems.length}</Button>
+      </OverlayTrigger>
 
       <Tab.Container activeKey={activeTab} onSelect={handleTabChange}>
         <div className="row">
