29b3e56289cd9810c8eeecd36834655ed4510562
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-05-21 13:52:16 -0300

    Move h1 to top

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import "./App.css";
 import BarraLateral from "./components/componentBarraLateral/barraLateral";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Checkout from './Checkout';
-
+import Checkout from "./Checkout";
 
 function App() {
   return (
     <div>
+      <h1 className="titulo">BurgerPlanet</h1>
       <Router>
         <Routes>
           <Route exact path="/" element={<BarraLateral />} />
           <Route exact path="/checkout" element={<Checkout />} />
         </Routes>
       </Router>
-
-      <h1 className="titulo">BurgerPlanet</h1>
 
       <main>{/* Other components or content */}</main>
       <footer>{/* Footer component or content */}</footer>
