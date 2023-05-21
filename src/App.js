59d37f778f77e48c879639a0d877832d9ee75ba1
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-05-21 13:50:06 -0300

    Config route /checkout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import './App.css';
-import BarraLateral from './components/componentBarraLateral/barraLateral';
-import React from 'react';
+import "./App.css";
+import BarraLateral from "./components/componentBarraLateral/barraLateral";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from './Checkout';
+
 
 function App() {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<BarraLateral />} />
+          <Route exact path="/checkout" element={<Checkout />} />
+        </Routes>
+      </Router>
+
       <h1 className="titulo">BurgerPlanet</h1>
-      <BarraLateral />
-      <main>
-        {/* Other components or content */}
-      </main>
-      <footer>
-        {/* Footer component or content */}
-      </footer>
+
+      <main>{/* Other components or content */}</main>
+      <footer>{/* Footer component or content */}</footer>
     </div>
   );
 }
 
 export default App;
-
---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Checkout() {
+  return (
+    <div>
+      <h1>Página de checkout</h1>
+      {/* Contenido de la página de checkout */}
+    </div>
+  );
+}
+
+export default Checkout;
