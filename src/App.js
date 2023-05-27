2c6e3539358eabab6e217c46cb0e47917f99ff80
rodrikraus <rodrikraus10@hotmail.com>
2023-05-27 16:56:15 -0300

    Mejorado la visual del logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import Checkout from "./Checkout";
 function App() {
   return (
     <div>
-      <h1 className="titulo">BurgerPlanet</h1>
+      <h1 className="titulo">
+        <img src={"https://i.ibb.co/pdbcDkX/logo-sin-bp.png"} alt="BurgerPlanet Logo" className="logo" /> BurgerPlanet
+      </h1>
       <Router>
         <Routes>
           <Route exact path="/" element={<BarraLateral />} />
